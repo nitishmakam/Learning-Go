1638e41bb089ee26072cafea2b2b42c2f6cc1923
Nitish Makam <nitishmakam@ITSupports-MBP.bbrouter>
2022-09-02 22:41:46 +0530

    Changes to rename the interface to Animal

--- Functions, Methods and Interfaces in Go/animal-info.go
+++ Functions, Methods and Interfaces in Go/animal-info.go
@@ -2,45 +2,83 @@ package main
 
 import "fmt"
 
-type animalInt interface {
+type Animal interface {
 	Eat()
 	Move()
 	Speak()
 }
 
-type animal struct {
+type Cow struct {
 	food       string
 	locomotion string
 	noise      string
 }
 
-func (ani animal) Eat() {
-	fmt.Println(ani.food)
+type Bird struct {
+	food       string
+	locomotion string
+	noise      string
 }
 
-func (ani animal) Move() {
-	fmt.Println(ani.locomotion)
+type Snake struct {
+	food       string
+	locomotion string
+	noise      string
 }
 
-func (ani animal) Speak() {
-	fmt.Println(ani.noise)
+func (cow Cow) Eat() {
+	fmt.Println(cow.food)
+}
+
+func (bird Bird) Eat() {
+	fmt.Println(bird.food)
+}
+
+func (snake Snake) Eat() {
+	fmt.Println(snake.food)
+}
+
+func (cow Cow) Move() {
+	fmt.Println(cow.locomotion)
+}
+
+func (bird Bird) Move() {
+	fmt.Println(bird.locomotion)
+}
+func (snake Snake) Move() {
+	fmt.Println(snake.locomotion)
+}
+func (cow Cow) Speak() {
+	fmt.Println(cow.noise)
+}
+
+func (bird Bird) Speak() {
+	fmt.Println(bird.noise)
+}
+
+func (snake Snake) Speak() {
+	fmt.Println(snake.noise)
 }
 
 func main() {
-	animalMap := make(map[string]animal)
-	animalMap["cow"] = animal{"grass", "walk", "moo"}
-	animalMap["bird"] = animal{"worms", "fly", "peep"}
-	animalMap["snake"] = animal{"mice", "slither", "hsss"}
+	animalToNameMap := make(map[string]Animal)
 
-	animalToNameMap := make(map[string]animal)
-
-	var genericAnimal animalInt
+	var genericAnimal Animal
 	for {
 		var op, ani, reqType string
 		fmt.Print(">")
 		fmt.Scan(&op, &ani, &reqType)
 		if op == "newanimal" {
-			animalToNameMap[ani] = animalMap[reqType]
+			var animalObj Animal
+			switch reqType {
+			case "cow":
+				animalObj = Cow{"grass", "walk", "moo"}
+			case "bird":
+				animalObj = Bird{"worms", "fly", "peep"}
+			case "snake":
+				animalObj = Snake{"mice", "slither", "hsss"}
+			}
+			animalToNameMap[ani] = animalObj
 			fmt.Println("Created it!")
 		}
 		if op == "query" {
